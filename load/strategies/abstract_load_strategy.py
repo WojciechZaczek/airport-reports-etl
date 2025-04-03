e050fb9628c8e101a1ccda65641e75c09d8ce550
github-WojciechZaczek <wojciech.zaczek84@gmail.com>
2025-04-03 19:35:06 +0200

    build: create load strategy and fix a1 strategy bug

--- load/strategies/abstract_load_strategy.py
+++ load/strategies/abstract_load_strategy.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 
 class LoadStrategy(ABC):
     @abstractmethod
-    def load(self):
+    def load(self, save_path: str):
         """
              This is an abstract method that should be implemented by subclasses.
 
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from extract.strategies.total_table_extract_strategy import TotalTableExtractStr
 from transform.strategies.gus_a1.gus_a1_transform_strategy import A1TransformStrategy
 from transform.strategies.gus_b1.gus_b1_transform_strategy import B1TransformStrategy
 from transform.strategies.gus_c1.gus_c1_transform_strategy import C1TransformStrategy
+from load.strategies.excel_load_strategy import ExcelLoadStrategy
 import os
 
 file_path_total = r"D:\Raporty\TABELA_TOTAL_AKTUALNA.xlsx"
@@ -28,10 +29,21 @@ a1_transformer = A1TransformStrategy(inflot_report_df, total_report_df)
 a1_transformer.run()
 
 b1_transformer = B1TransformStrategy(a1_transformer.df_a1)
-b1_transformer.run().get_data()
+b1_transformer.run()
 
 c1_transformer = C1TransformStrategy(inflot_report_df, total_report_df)
 c1_transformer.run()
+
+year, month = a1_transformer.get_year_month()
+
+loader_a1 = ExcelLoadStrategy(a1_transformer.run(), "A1", year, month)
+loader_a1.load(r'C:\Users\wojci\PycharmProjects\airport-reports-etl\boxes\GUS\reports')
+
+loader_b1 = ExcelLoadStrategy(b1_transformer.run(), "B1", year, month)
+loader_b1.load(r'C:\Users\wojci\PycharmProjects\airport-reports-etl\boxes\GUS\reports')
+
+loader_c1 = ExcelLoadStrategy(c1_transformer.run(),"C1", year, month)
+loader_c1.load(r'C:\Users\wojci\PycharmProjects\airport-reports-etl\boxes\GUS\reports')
 
 
 
